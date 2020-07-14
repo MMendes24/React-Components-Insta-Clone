d8f4bdc823db2318615d41ee72cbdc8f80e26bc3
MMendes24 <mmbusiness1@protonmail.com>
2020-07-14 14:05:49 -0400

    Imports complete.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ Look at each file to see what props need to be passed.
 */
 
 // Import the state hook
-import React from "react";
+import React, {useState} from "react"
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from "./components/Posts/Posts"
+import SearchBar from "./components/SearchBar/SearchBar"
 // Import the dummyData
-import "./App.css";
+import dummyData from "./dummy-data"
+import "./App.css"
+import Post from "./components/Posts/Post"
 
 const App = () => {
   // Create a state called 'posts' to hold the list of posts, initializing to dummyData.
+  const [posts] = useState(dummyData)
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = postId => {
@@ -25,7 +30,8 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* Add SearchBar and Posts here to render them */}
+      { <SearchBar />}
+      { <Posts />}
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
